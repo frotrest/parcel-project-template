f35c7acabb0a9f0994007a7d4d61ea48abd1a0e8
TVOROG5776 <romansydorko10@gmail.com>
2025-11-14 12:11:32 +0200

    final-fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,27 @@ import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default [
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ["**", "!src/**/*.{js,jsx,ts,tsx}"],
 
-    files: ["src/**/*.{js,jsx,ts,tsx}", ".github/**/*.yml"],
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
 
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
       globals: {
         window: "readonly",
-        document: "readonly",
-      },
+        document: "readonly"
+      }
     },
 
     plugins: {
-      prettier: eslintPluginPrettier,
+      prettier: eslintPluginPrettier
     },
 
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
-      "prettier/prettier": "warn",
-    },
-  },
+      "prettier/prettier": "warn"
+    }
+  }
 ];
